46a23e292a3c096b8cb4cbec1051c08ded06fb79
Cyro <BlueCyro@protonmail.com>
2023-11-27 18:58:55 -0600

    Initial Commit

---
+++ ResoniteModTemplate.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+using FrooxEngine;
+using Elements.Core;
+
+namespace TemplateNamespace;
+
+public class ResoniteModTemplate : ResoniteMod
+{
+    public override string Name => "Resonite Mod Template";
+    public override string Author => "Owner";
+    public override string Version => "1.0.0";
+    public override string Link => "resonite.com";
+    public static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.Owner.ResoniteModTemplate");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
